cb594983fa10b36243802fa73585f87f31662d48
Aryan Kulathinal <aryansajankulathinal@gmail.com>
2025-01-21 03:00:52 +0530

    frontend update

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { TeamMemberDashboardComponent } from './pages/team-member-dashboard/team-member-dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: TeamMemberDashboardComponent },
+  // Add more routes as needed
+];
---
+++ frontend/src/app/pages/team-member-dashboard/team-member-dashboard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from '../../shared/navbar/navbar.component';
+
+@Component({
+  selector: 'app-team-member-dashboard',
+  standalone: true,
+  imports: [NavbarComponent],
+  templateUrl: './team-member-dashboard.component.html',
+  styleUrl: './team-member-dashboard.component.scss'
+})
+export class TeamMemberDashboardComponent {
+}
