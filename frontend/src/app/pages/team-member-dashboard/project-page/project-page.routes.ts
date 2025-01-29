553a01dba8c063c623463fe72da33f75d4cc8ab8
AryanKulathinal <aryansajankulathinal@gmail.com>
2025-01-29 14:19:53 +0530

    forntend update

--- frontend/src/app/pages/team-member-dashboard/project-page/project-page.routes.ts
+++ frontend/src/app/pages/team-member-dashboard/project-page/project-page.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { ProjectPageComponent } from './project-page.component';
 import { OverviewComponent } from './overview/overview.component';
 import { MyTeamComponent } from './my-team/my-team.component';
+import { UnderConstructionComponent } from '../../../pages/under-construction/under-construction.component';
 
 export const PROJECT_ROUTES: Routes = [
   {
@@ -16,6 +17,18 @@ export const PROJECT_ROUTES: Routes = [
       {
         path: 'overview',
         component: OverviewComponent
+      },
+      {
+        path: 'objectives',
+        component: UnderConstructionComponent
+      },
+      {
+        path: 'tasks',
+        component: UnderConstructionComponent
+      },
+      {
+        path: 'calendar',
+        component: UnderConstructionComponent
       },
       {
         path: 'my-team',
