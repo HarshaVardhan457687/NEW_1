b53e5ffa98a0fd6f692ca645a0d51f4e46a6020d
chaitanyakatore <chaitanyakatore@gmail.com>
2025-03-05 16:52:58 +0530

    updated backend

--- Backend/objective-service/src/main/java/com/objective/objective_service/config/MyConfig.java
+++ Backend/objective-service/src/main/java/com/objective/objective_service/config/MyConfig.java
@@ -2,6 +2,7 @@ package com.objective.objective_service.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -9,7 +10,7 @@ public class MyConfig {
 
     @Bean
     public RestTemplate restTemplate() {
-        return new RestTemplate();
+        return new RestTemplate(new HttpComponentsClientHttpRequestFactory());
     }
 }
 
