206b8e5af7b3f8a8045075bb4ccc10143c29d9b4
chaitanyakatore <chaitanyakatore@gmail.com>
2025-03-07 18:37:51 +0530

    update Backend

--- Backend/task-service/src/main/java/com/task/task_service/controller/TaskApprovalController.java
+++ Backend/task-service/src/main/java/com/task/task_service/controller/TaskApprovalController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/approvals")
+@RequestMapping("/api/approvals")
 public class TaskApprovalController {
 
     @Autowired
